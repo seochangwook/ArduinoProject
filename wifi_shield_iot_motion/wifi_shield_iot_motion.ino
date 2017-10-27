e159d26dd3f08c77fbc696f6ef22cd7e02bb2ab6
SeoChangWook <scw05313315@gmail.com>
2017-10-27 22:31:29 +0900

    home iot project - motion

---
+++ wifi_shield_iot_motion/wifi_shield_iot_motion.ino
@@ -0,0 +1,103 @@
+#include <WiFi.h>
+
+char ssid[] = "KT_GiGA_2G_Family";     //  your network SSID (name) 
+char pass[] = "ehd0134679";    // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+
+  // attempt to connect using WPA2 encryption:
+  Serial.println("Attempting to connect to WPA network...");
+  status = WiFi.begin(ssid, pass);
+
+  // 현재 아두이노에 연결된 실드를 확인
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    // 와이파이 실드가 아닐 경우
+    while(true);
+  } else{
+    Serial.println("wifi shield is load...");
+  }
+
+  // check firmware version
+  Serial.print(F("Firmware version: "));
+  Serial.println(WiFi.firmwareVersion());
+  
+  // if you're not connected, stop here:
+  if ( status != WL_CONNECTED) { 
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  } 
+  // if you are connected, print out info about the connection:
+  else {
+    Serial.println("Connected to network");
+
+    Serial.println("--------------------------");
+    printWifiData();
+    Serial.println("--------------------------");
+    printCurrentNet();
+  }
+}
+
+void loop() {
+  // do nothing
+}
+//////////////////////////
+void printWifiData() {
+  // WI-FI 실드의 IP를 출력한다.
+  IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+  
+  // MAC어드레스를 출력한다.
+  byte mac[6];  
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+}
+
+void printCurrentNet() {
+  // 접속하려는 네트워크의 SSID를 출력한다.
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // 접속하려는 router의 MAC 주소를 출력한다.
+  byte bssid[6];
+  WiFi.BSSID(bssid);    
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5],HEX);
+  Serial.print(":");
+  Serial.print(bssid[4],HEX);
+  Serial.print(":");
+  Serial.print(bssid[3],HEX);
+  Serial.print(":");
+  Serial.print(bssid[2],HEX);
+  Serial.print(":");
+  Serial.print(bssid[1],HEX);
+  Serial.print(":");
+  Serial.println(bssid[0],HEX);
+
+  // 수신 신호 강도를 출력한다.
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // 암호화 타입을 출력한다.
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption,HEX);
+  Serial.println();
+}
